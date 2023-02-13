4597a49d0bbedc9e328d3792c6dabda54999d6e3
Vale <oirelav95@gmail.com>
2023-02-13 17:14:42 +0100

    aggiungo chiamate dei seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this -> call([
+
+            PersonSeeder::class,
+            PersonDetailSeeder::class, 
+            PostSeeder::class,
+        ]);
     }
 }
