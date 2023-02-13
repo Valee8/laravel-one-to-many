48eb5c446ec9e09d5bfe3d42ad65fef43c6c3045
Vale <oirelav95@gmail.com>
2023-02-13 17:12:18 +0100

    creo MainController

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Person;
+use App\Models\Post;
+
+class MainController extends Controller
+{
+    public function home() {
+
+        $people = Person::all();
+
+        return view('pages.home', compact('people'));
+    }
+}
